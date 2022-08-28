6f949ce6e4d1b0b1b8f544e4e212831fc8a9452e
Artur Souza <artursouza.ms@outlook.com>
2022-08-27 17:35:58 -0700

    Adding web UI + some clean up.
    Signed-off-by: Artur Souza <artursouza.ms@outlook.com>

--- webapp/main.py
+++ webapp/main.py
@@ -15,28 +15,37 @@ dapr = DaprClient()
 @app.route("/", methods=["POST"])
 def shorten():
     """Creates a shortened URL from a full URL."""
-    url=request.args.get('url')
-    shortId=generateRandomShortId()
-    print("Setting", shortId, "=>", url, "...", flush=True)
-    dapr.save_state(store_name=DAPR_STATE_STORE, key=shortId, value=url)
-    print("Set", shortId, "=>", url, flush=True)
-    return str(request.root_url + shortId)
+    url=request.form["url"]
+    if not url:
+        return redirect("/")
+    short_id=generate_random_short_id()
+    print("Setting", short_id, "=>", url, "...", flush=True)
+    dapr.save_state(store_name=DAPR_STATE_STORE, key=short_id, value=url)
+    print("Set", short_id, "=>", url, flush=True)
+    short_url = request.root_url + short_id
+    return f"<a href='{short_url}'>{short_url}</a>"
 
-@app.route("/<string:shortId>")
-def resolve(shortId):
+@app.route("/<string:short_id>")
+def resolve(short_id):
     """Redirects to URL."""
-    print("Resolving", shortId, "...", flush=True)
-    state = dapr.get_state(store_name=DAPR_STATE_STORE, key=shortId)
+    print("Resolving", short_id, "...", flush=True)
+    state = dapr.get_state(store_name=DAPR_STATE_STORE, key=short_id)
     url = state.data.decode()
-    print("Resolved", shortId, "=>", url, flush=True)
+    print("Resolved", short_id, "=>", url, flush=True)
     return redirect(url)
 
 @app.route("/")
 def index():
-    return "This is running!"
+    return """<form action="" method="POST">
+                <input type="text" name="url" size="80">
+                <input type="submit" value="Submit">
+              </form>"""
 
-def generateRandomShortId():
-    return "".join(map(lambda i:chr(i), random.choices(range(65,91), k=SHORT_URL_ID_LENGTH)))
+def generate_random_short_id():
+    return "".join(map(
+        lambda i:chr(i),
+        random.choices(range(65,91),
+        k=SHORT_URL_ID_LENGTH)))
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080)
